a287c53e4195f79dc06c35a2760330f2fe23ac8e
Nikita Timofeev <ndtimofeev@gmail.com>
2010-11-03 00:19:42 +0300

    Срд Ноя  3 00:19:42 MSK 2010

---
+++ src/spectrumwindow/curvesettingsdialog.cpp
@@ -0,0 +1,13 @@
+#include "curvesettingsdialog.h"
+#include "ui_curvesettingsdialog.h"
+
+CurveSettingsDialog::CurveSettingsDialog( QWidget* parent ) : QWidget( parent )
+{
+    Ui::CurveSettingsDialog ui;
+    ui.setupUi( this );
+}
+
+CurveSettingsDialog::~CurveSettingsDialog()
+{
+}
+
---
+++ src/spectrumwindow/curvesettingsdialog.h
@@ -0,0 +1,16 @@
+#ifndef CURVESETTINGSDIALOG_H
+#define CURVESETTINGSDIALOG_H
+
+#include <QWidget>
+
+class CurveSettingsDialog : public QWidget
+{
+    Q_OBJECT
+
+public:
+    CurveSettingsDialog( QWidget* parent = 0 );
+    virtual ~CurveSettingsDialog();
+};
+
+#endif /* CURVESETTINGSDIALOG_H */
+
