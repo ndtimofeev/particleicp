01c8fbd6d8a2b38da3d203c269ddf11f61b13bfb
Nikita Timofeev <ndtimofeev@gmail.com>
2010-12-11 03:24:57 +0300

    Сбт Дек 11 03:24:57 MSK 2010

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,10 @@ int main( int argc, char** argv )
     QApplication app(argc, argv);
 
         Settings::init( "PsiLab", "ParticleICP" );
-
         QTranslator qtTranslator;
-
         qtTranslator.load("particleicp_" + QLocale::system().name());
-//                        QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-
-        qDebug() << QLibraryInfo::location(QLibraryInfo::TranslationsPath);
-        qDebug() << QLocale::system().name();
-
         app.installTranslator(&qtTranslator);
-
         MainWindow mw;
-
         mw.show();
 
     return app.exec();
--- src/reuseable/rescaledialog.cpp
+++ src/reuseable/rescaledialog.cpp
@@ -1,4 +1,5 @@
 #include <QDebug>
+#include <qwt_plot_dict.h>
 #include "rescaledialog.h"
 #include "ui_rescaledialog.h"
 
@@ -18,6 +19,18 @@ RescaleDialog::RescaleDialog( QwtPlot* plot, QWidget* parent ) :
 
     ui.checkBoxX->setChecked( plot->axisAutoScale( QwtPlot::xBottom ) );
     ui.checkBoxY->setChecked( plot->axisAutoScale( QwtPlot::yLeft ) );
+
+    ui.rangeBoxX->setRange( plot->canvasMap( QwtPlot::xBottom ).s1(),
+                                    plot->canvasMap( QwtPlot::xBottom ).s2() );
+
+    ui.rangeBoxX->setMinValue( plot->canvasMap( QwtPlot::xBottom ).s1() );
+    ui.rangeBoxX->setMaxValue( plot->canvasMap( QwtPlot::xBottom ).s2() );
+
+    ui.rangeBoxY->setRange( plot->canvasMap( QwtPlot::yLeft ).s1(),
+                                    plot->canvasMap( QwtPlot::yLeft ).s2() );
+
+    ui.rangeBoxY->setMinValue( plot->canvasMap( QwtPlot::yLeft ).s1() );
+    ui.rangeBoxY->setMaxValue( plot->canvasMap( QwtPlot::yLeft ).s2() );
 }
 
 RescaleDialog::~RescaleDialog()
